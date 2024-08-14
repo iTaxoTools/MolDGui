19394d8cc3851846c2657d7cf290750df70dde47
StefanPatman <stefanpatman91@gmail.com>
2024-08-14 15:59:15 +0300

    Upgrade building toolkit

--- tools/run.py
+++ tools/run.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Launch the MolD GUI"""
+"""Launch the Hapsolutely GUI"""
 
 import multiprocessing
 
 from itaxotools.mold.gui import run
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     multiprocessing.freeze_support()
     run()
